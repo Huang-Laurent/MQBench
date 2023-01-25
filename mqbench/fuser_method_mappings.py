e5a2f8a1828c0b35d924856c48251ad4614cba48
61HzPentile <laurent.huang@outlook.fr>
2023-01-25 13:29:38 +0100

    fuse

--- mqbench/fuser_method_mappings.py
+++ mqbench/fuser_method_mappings.py
@@ -2,8 +2,10 @@ from typing import Optional, Type
 
 import torch
 import torch.nn as nn
-from torch.ao.quantization.fx.fusion_patterns import ConvBNReLUFusion, ModuleReLUFusion
-from torch.ao.quantization.fx.quantization_types import QuantizerCls
+# from torch.ao.quantization.fx.fusion_patterns import ConvBNReLUFusion, ModuleReLUFusion
+# from torch.ao.quantization.fx.quantization_types import QuantizerCls
+from torch.quantization.fx.fusion_patterns import ConvBNReLUFusion, ModuleReLUFusion
+from torch.quantization.fx.quantization_types import QuantizerCls
 from torch.fx.graph import Node
 
 import mqbench.nn as qnn
